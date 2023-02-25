94ce1d100c07b365231510fbe20c4509feebbad0
samuel_tuki <smauel_tuki@yahoo.com>
2023-02-25 07:30:44 +0000

    license badge created

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 
-  # s
+  # DOD
   ## Description
-  s
+  D
   ## Table Of Content
-  *[title](#title)
-  *[description](#description)
-  *[installation](#installation)
-  *[usage](#usage)
-  *[license](#license)
-  *[contributor](#contributor)
-  *[test](#test)
+  *[Title](#title)
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributor](#contributor)
+  *[Test](#test)
+  *[Questions](#questions)
   ## Installation
   undefined
   ## How is your project used?
-  s
+  D
   ## license
-  
-  ## Contributors
-  s
-  ## Tests
-  s
-  ## Questions
-  Any questions about this project please send me a message on https://github.com/s or email me at [s](mailto:s)
+  [![License: MIDDLEWARE](https://img.shields.io/badge/License-MIDDLEWARE-brightgreen.svg)](https://opensource.org/licenses/MIDDLEWARE)
 
+
+  ## Contributors
+  D
+  ## Tests
+  D
+  ## Questions
+  Any questions about this project please send me a message on https://github.com/D or email me at [D](mailto:D)
+  ## License
+  This project is licensed under the MIDDLEWARE license.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,44 @@
 // function to generate markdown for README
+
 function generateMarkdown(data) {
+  let licenseBadge = '';
+  if (data.license) {
+     // If there is a license specified in the data, create a license badge
+    licenseBadge = `[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-brightgreen.svg)](https://opensource.org/licenses/${data.license})\n\n`;
+  }
+
+// variable that generate the readMe file
   const readMe = `
   # ${data.title}
   ## Description
   ${data.description}
   ## Table Of Content
-  *[title](#title)
-  *[description](#description)
-  *[installation](#installation)
-  *[usage](#usage)
-  *[license](#license)
-  *[contributor](#contributor)
-  *[test](#test)
+  *[Title](#title)
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributor](#contributor)
+  *[Test](#test)
+  *[Questions](#questions)
   ## Installation
   ${data.installation}
   ## How is your project used?
   ${data.usage}
   ## license
-  ${data.license}
+  ${licenseBadge}
   ## Contributors
   ${data.contributor}
   ## Tests
   ${data.test}
   ## Questions
   Any questions about this project please send me a message on https://github.com/${data.github} or email me at [${data.email}](mailto:${data.email})
-
+  ${data.license ? `## License
+  This project is licensed under the ${data.license} license.` : ''}
 `
 return readMe;
 }
 
 
-
-
-
+// exporting file to index.js
 module.exports = generateMarkdown;
